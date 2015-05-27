b9fe29c116da9608ce3ae48a8834d9ff3119b8fb
mem0ow <turk0ow_mem0ow@live.nl>
2015-05-27 12:05:29 +0200

    new commit

--- src/doolhof/SpelStat.java
+++ src/doolhof/SpelStat.java
@@ -8,8 +8,11 @@ package doolhof;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 
 /**
  *
@@ -17,9 +20,13 @@ import javax.swing.JPanel;
  */
 public class SpelStat extends JPanel {
     
+    JLabel label;
     JButton startB;
     JButton restartB;
     Level level;
+        TimerTask task;
+    Timer timer;
+    Speler speler;
     
     public SpelStat()
     {
@@ -35,6 +42,9 @@ public class SpelStat extends JPanel {
         restartB.setBounds(110, 10, 100, 30);
         add(startB);
         startB.setBounds(10, 10, 100, 30);
+        label = new JLabel("60");
+        add(label);
+        label.setBounds(250, 10, 100, 30);
     }
     
     public class ResetListnenr implements ActionListener
@@ -44,6 +54,9 @@ public class SpelStat extends JPanel {
         {
 
             level.starten();
+            task = new Task();
+            timer = new Timer();
+            timer.schedule(task, 1000, 1000);
         }
  }
     
@@ -53,8 +66,27 @@ public class SpelStat extends JPanel {
         public void actionPerformed(ActionEvent e) 
         {
             level.herstarten();
+            timer.cancel();
+            label.setText("60");
+            requestFocus();
             
         }
  }
+     public class Task extends TimerTask {
+
+        public int countdown = 59;
+
+        @Override
+        public void run() {
+            String tijd = Integer.toString(countdown);
+            label.setText(tijd);
+            if(countdown==0){
+            timer.cancel();
+            requestFocus();}
+            else{
+            countdown -= 1;}
+            
+
+        }
     
-}
+}}
